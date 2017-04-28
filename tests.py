7e9043f2a17d946a69ac5b5072375297052c592a
rosz <anna.roszman@gmail.com>
2017-04-28 12:27:15 +0200

    tests file removed

--- tests.py
+++
@@ -1,9 +0,0 @@
-from tornado.testing import AsyncHTTPTestCase
-import rest_api
-import json
-
-
-class TestRestApi(AsyncHTTPTestCase):
-    # tests for the method adding up numbers
-    def get_app(self):
-        return rest_api.make_app()
